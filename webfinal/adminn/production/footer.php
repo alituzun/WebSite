0a3be414494bd0481b93b80b3980d812a23c3612
alituzun <38184161+alituzun@users.noreply.github.com>
2019-03-13 16:27:58 +0300

    Create footer.php

---
+++ webfinal/adminn/production/footer.php
@@ -0,0 +1,24 @@
+ <!-- footer content -->
+        <footer>
+          <div class="pull-right">
+           Ali tüzün (ahref'e facebook adresi) <a href="https://colorlib.com">Ali tüzün</a>
+          </div>
+          <div class="clearfix"></div>
+        </footer>
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+   
+    <!-- Bootstrap -->
+    <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="../vendors/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="../vendors/nprogress/nprogress.js"></script>
+    
+    <!-- Custom Theme Scripts -->
+    <script src="../build/js/custom.min.js"></script>
+  </body>
+</html>
